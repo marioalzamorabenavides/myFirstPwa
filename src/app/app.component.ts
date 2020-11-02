97d566f3159c050bd114d2e81442969c483c629a
unknown <marioalzamorabenavides@gmail.com>
2020-11-02 16:14:51 -0500

    add button install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,25 @@ import { SwUpdate } from '@angular/service-worker';
 export class AppComponent {
 
   title = 'serviceWorkerExample';
-
+  showButtonInstall = false;
+  deferredPrompt;
+  
   constructor(private serviceWorker: SwUpdate) {
+
+    window.addEventListener('appinstalled', (evt) => {
+      // Log install to analytics
+      console.log('INSTALL: Success');
+    });
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      // showInstallPromotion();
+      this.showButtonInstall = true;
+    });
 
     if (serviceWorker.isEnabled) {
 
@@ -22,5 +39,18 @@ export class AppComponent {
         }
       });
     }
+  }
+
+  installApp() {
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    });
+
   }
 }
